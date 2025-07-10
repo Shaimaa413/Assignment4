afd0b11f6d26a178fbf7c2111eaa04007b45705e
Shaimaa413 <shaimaamansour413@gmail.com>
2025-07-10 17:12:07 +0300

    Assignment4

---
+++ src/Assignment4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Assignment4 {
+    public static void main(String[]args){
+        Scanner input= new Scanner(System.in);
+                System.out.print("Enter Your Age: ");
+    int age= input.nextInt();
+            input.nextLine();
+
+            System.out.print("Enter Your Nationality:  ");
+            String nationality = input.nextLine();
+
+            if (age>= 18 && nationality.equals("Egyption"))
+            { System.out.println("Eligible to Vote");
+            }
+            else {
+                System.out.println("Not Eligible to Vote");
+            }
+    }
+}
